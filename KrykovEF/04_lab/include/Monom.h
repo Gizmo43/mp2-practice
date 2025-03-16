73e711319c03a00e13d57c266051c97d414a3b87
Gizmo43 <egarus2005@gmail.com>
2025-03-16 21:54:28 +0300

    double check

--- KrykovEF/04_lab/include/Monom.h
+++ KrykovEF/04_lab/include/Monom.h
@@ -11,7 +11,37 @@ public:
 	int degree;
 	Monom() : coef(0), degree(0) {};
 	Monom(double coef, int degree) :coef(coef), degree(degree) {};
+    Monom operator+(const Monom& m) {
+        if (degree != m.degree)
+            throw exception("wrong degree");
+        return Monom(coef + m.coef, degree);
+    }
+    Monom operator-(const Monom& m) {
+        if (degree != m.degree)
+            throw exception("wrong degree");
+        return Monom(coef - m.coef, degree);
+    }
+    Monom operator*(const Monom& m) {
+        if (degree != m.degree)
+            throw exception("wrong degree");
+        return Monom(coef * m.coef, degree * 2);
+    }
+    Monom operator*(double m) {
+        return Monom(coef * m, degree);
+    }
+    const Monom& operator=(const Monom& m) {
+        return Monom(m.coef, m.degree);
+    }
+    bool operator==(const Monom& m) const
+    {
+        return ((coef == m.coef) && (degree == m.degree));
+    }
+    bool operator!=(const Monom& m) const
+    {
+        return(!(operator==(m)));
+    }
 };
+
 
 
 #endif
